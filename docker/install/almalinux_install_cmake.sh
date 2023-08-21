f8c82f1ba42a83682cec688d1126224ea84b8cd3
Robin <choronz@live.com>
2023-08-21 15:28:45 +0800

    2

--- docker/install/almalinux_install_cmake.sh
+++ docker/install/almalinux_install_cmake.sh
@@ -8,6 +8,7 @@ dnf makecache --refresh
 dnf -y install cmake
 dnf -y --enablerepo=powertools install ninja-build
 
+
 # Fix for latest version linux
 # dnf install -y dnf-plugin-config-manager
 # dnf config-manager --set-enabled crb
